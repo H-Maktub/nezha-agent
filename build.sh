f6d668d586bc49fbac7a90629c73295b1fe6b9c3
Maktub H <10336453+H-Maktub@users.noreply.github.com>
2026-05-07 00:02:23 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 rm ./bin/*
-Version=1.14.1
+go mod tidy
+Version=2.0.3
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build  -ldflags "-s -w -X github.com/nezhahq/agent/pkg/monitor.Version=$Version -X main.arch=amd64" -o ./bin/nezha-agent.exe cmd/agent/main.go
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -ldflags "-s -w -X github.com/nezhahq/agent/pkg/monitor.Version=$Version -X main.arch=amd64" -o ./bin/nezha-agent cmd/agent/main.go
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build  -ldflags "-s -w -X github.com/nezhahq/agent/pkg/monitor.Version=$Version -X main.arch=arm64" -o ./bin/nezha-agent-arm cmd/agent/main.go
-CGO_ENABLED=0 GOOS=linux GOARCH=mipsle GOMIPS=softfloat go build  -ldflags "-s -w -X github.com/nezhahq/agent/pkg/monitor.Version=$Version -X main.arch=mipsle" -o ./bin/nezha-agent-jd-t cmd/agent/main.go
-upx --best -o ./bin/nezha-agent-jd ./bin/nezha-agent-jd-t
-rm ./bin/nezha-agent-jd-t
