c61155f5e0fe0a3ae4a2dfa32c6a28af1d7195d4
Maktub_小明 <10336453+H-Maktub@users.noreply.github.com>
2026-07-04 18:46:27 +0800

    兼容

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 rm ./bin/*
+go clean -modcache
 go mod tidy
 Version=2.2.3
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build  -ldflags "-s -w -X github.com/nezhahq/agent/pkg/monitor.Version=$Version -X main.arch=amd64" -o ./bin/nezha-agent.exe cmd/agent/main.go
